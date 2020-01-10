a44797c67bf8b29f70e984bd7eb0b357b22651f2
brahim <brahim@brahim-Latitude-E6520>
2020-01-10 13:23:23 +0100

    THIS COMMIT

---
+++ AGP-ECLIPSE/src/main/java/org/o7planning/AGP_ECLIPSE/App.java
@@ -0,0 +1,13 @@
+package org.o7planning.AGP_ECLIPSE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello BRAHIM !" );
+    }
+}
